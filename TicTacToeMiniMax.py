7fde75bde70b916651f060318575e6a0358fd37e
AkilanChithra <chithra.sathish.akilan@gmail.com>
2024-07-10 00:41:49 -0500

    Changed code to have AI autopick a random move, and winstate works

--- TicTacToeMiniMax.py
+++ TicTacToeMiniMax.py
@@ -1,79 +1,97 @@
-import tkinter, time
-
-
+import tkinter, time, random
+from tkinter import messagebox
 
 def set_tile(row, col):
-    global currPlayer
-    
-    if game_over:
+    global currPlayer, gameOver, moves
+
+    if gameOver:
         return
-    
     if board[row][col]["text"] !="":    
         return
-    
-    board[row][col]["text"] = currPlayer
-    
-    
 
-    if currPlayer == playerO:
-        currPlayer = playerX
-    else:
-        currPlayer = playerO
-        aiMove()
+    board[row][col]["text"] = PlayerSign
+    
+    moves+=1
+    if moves>4:checkWinner()
+    currPlayer = 1
 
-    label['text'] = currPlayer+"'s turn"
-    # print("check winner")
-      
-    checkWinner()
+    aiMove()
+
 def aiMove():
-    print("inside Ai move")
-    pass
+    global moves, gameOver, currPlayer
+    if gameOver:
+        return
+    movePicked = False
+    while(movePicked == False):
+        randRow = random.randint(0,2)
+        randCol = random.randint(0,2)
+        if board[randRow][randCol]["text"] == "":
+            board[randRow][randCol]["text"] = AISign
+            movePicked = True
+    if moves>4:checkWinner()
+    moves+=1
+    movePicked = False
+    currPlayer = 0
+
 def checkWinner():
-    global turns, game_over
-    
+    global gameOver
+    print("check winner")
+    for i in range(3):
+        if board[i][0]["text"] == board[i][1]["text"] == board[i][2]["text"] and board[i][0]["text"]!="":
+            print("Player won")
+            if currPlayer == 0:
+                label['text'] = "YOU WIN!"
+            else:
+                label['text'] = "YOU LOSE"
+            gameOver = True
+            
+    for i in range(3):
+        if board[0][i]["text"] == board[1][i]["text"] == board[2][i]["text"] and board[0][i]["text"]!="":
+            print("Player won")
+            if currPlayer == 0:
+                label['text'] = "YOU WIN!"
+            else:
+                label['text'] = "YOU LOSE"
+            gameOver = True
+            
+    if board[0][0]["text"] == board[1][1]["text"] == board[2][2]["text"] and board[0][0]["text"]!="":
+        print("Player won")
+        if currPlayer == 0:
+            label['text'] = "YOU WIN!"
+        else:
+            label['text'] = "YOU LOSE"
+        gameOver = True
+        
+    if board[2][0]["text"] == board[1][1]["text"] == board[0][2]["text"] and board[2][0]["text"]!="":
+        print("Player won")
+        if currPlayer == 0:
+            label['text'] = "YOU WIN!"
+        else:
+            label['text'] = "YOU LOSE"
+        gameOver = True
+    if moves == 9 and gameOver == False:
+        gameOver = True
+        label['text'] = "TIE!!"
+        
 
-    for row in range(3):
-        if(board[row][0]["text"] == board[row][1]["text"] == board[row][2]["text"] and board[row][0]["text"]!= ""):
-            game_over = True
-            label.config(text = board[row][0]["text"]+" is the winner!", foreground=colorYellow)
-    for col in range(3):
-        if(board[0][col]["text"] == board[1][col]["text"] == board[2][col]["text"] and board[0][col]["text"]!= ""):
-            game_over = True
-            label.config(text = board[0][col]["text"]+" is the winner!", foreground=colorYellow)
-    if( board[0][0]["text"]!= "" and (board[0][0]["text"] == board[1][1]["text"] == board[2][2]["text"])):
-
-        game_over = True
-        label.config(text = board[0][0]["text"]+" is the winner!", foreground=colorYellow)
-    if(board[2][0]["text"]!="" and (board[2][0]["text"] == board[1][1]["text"] == board[0][2]["text"])):
-        game_over = True
-        label.config(text = board[2][0]["text"]+" is the winner!", foreground=colorYellow)
-    
-    turns+=1
-    if(turns == 9):
-        game_over == True
-        label.config(text="Tie!", foreground=colorYellow)
-    
-
-    # print("this is currPlayer: "+currPlayer)
 def new_game():
-    global turns, game_over
+    global moves, gameOver, currPlayer
     print("restart pressed")
     for row in range(3):
         for col in range(3):
             board[row][col]["text"] = ""
-            turns = 0
-            game_over = False
-            currPlayer = playerX
-            label.config(text = currPlayer+" 's turn", foreground="white")
+            moves = 0
+            gameOver = False
+            currPlayer = 0
 
-    print(game_over)
-    pass
+    label["text"] = "GAME ON!"
 
-
-
-playerX = 'X'
-playerO = 'O'
-currPlayer = playerX
+#0 is player, 1 is AI
+currPlayer = 0
+gameOver = False
+moves = 0
+PlayerSign = "X"
+AISign = "O"
 
 board = [[0,0,0],
          [0,0,0],
@@ -84,30 +102,30 @@ colorYellow = "#ffde57"
 colorGray = "#343434"
 colorLightGray = "646464"
 colorBlack = "#000000"
+
+
+#creates buttons
 window = tkinter.Tk()
-
-
 turns = 0
-game_over = False
-
 window.title("Tic Tac Toe")
 window.resizable(False, False)
-
 frame = tkinter.Frame(window)
-  
-label = tkinter.Label(frame, text = currPlayer+" 's turn", font = ("Consolas", 20), background = colorBlack, foreground = "white")
+label = tkinter.Label(frame, text = "GAME ON!", font = ("Consolas", 20), background = colorBlack, foreground = "white")
 label.grid(row=0, column=0, columnspan = 3, sticky = "we")
 for row in range(3): 
     for column in range(3):
-        board[row][column] = tkinter.Button(frame, text = "", font = ("Consolas", 50, "bold"), background = colorGray, foreground = colorBlue, width = 4, height = 2, command=lambda row=row, column = column: set_tile(row, column))
+        board[row][column] = tkinter.Button(frame, text = "", font = ("Consolas", 50, "bold"), bg = 'blue', foreground = colorBlue, width = 4, height = 2, command=lambda row=row, column = column: set_tile(row, column))
         board[row][column].grid(row=row+1, column = column)
 
 
 
-
-button = tkinter.Button(frame, text = "restart", font=("Consolas", 20), background = colorYellow, foreground=colorBlack, command= new_game)
+#restart game button
+button = tkinter.Button(frame, text = "RESTART", font=("Consolas", 20), background = colorYellow, foreground=colorBlack, command= new_game)
 button.grid(row=4, column=0, columnspan=3, sticky="we")#sticky stretches the button out to both sides
-#we means west to eats, ns means north to south
+#we means west to eats, ns means north to south``
+
+
+#Centers window
 frame.pack()
 window.update()
 window_width = window.winfo_width()
@@ -122,10 +140,4 @@ window_y = int((screen_height/2)-(window_height/2))
 window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 
-
-window.mainloop()# keeps window open even after code ends
-
-
-
-
-
+window.mainloop()
